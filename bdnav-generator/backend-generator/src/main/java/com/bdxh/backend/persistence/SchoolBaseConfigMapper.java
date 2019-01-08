56b008544675ba5c0ae2959c92232a03692724c3
litao <762118752@qq.com>
2019-01-08 10:25:16 +0800

    一卡通

---
+++ bdnav-generator/backend-generator/src/main/java/com/bdxh/backend/persistence/SchoolBaseConfigMapper.java
@@ -0,0 +1,7 @@
+package com.bdxh.backend.persistence;
+
+import com.bdxh.backend.entity.SchoolBaseConfig;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SchoolBaseConfigMapper extends Mapper<SchoolBaseConfig> {
+}
