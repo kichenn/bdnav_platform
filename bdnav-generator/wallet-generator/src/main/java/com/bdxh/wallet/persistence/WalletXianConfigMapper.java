d627df33f9cfc6ac3ca769044e4d7f330ddd5e7a
xuyuan <532097775@qq.com>
2019-01-08 17:32:59 +0800

    配置改动

---
+++ bdnav-generator/wallet-generator/src/main/java/com/bdxh/wallet/persistence/WalletXianConfigMapper.java
@@ -0,0 +1,7 @@
+package com.bdxh.wallet.persistence;
+
+import com.bdxh.wallet.entity.WalletXianConfig;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface WalletXianConfigMapper extends Mapper<WalletXianConfig> {
+}
