855120dec0c5293fd158b19b12204fd8336d32ff
litao <762118752@qq.com>
2019-01-17 11:48:17 +0800

    '改动参数'

--- bdnav-common/common-base/src/main/java/com/bdxh/common/base/constant/WxAuthorizedConstants.java
+++ bdnav-common/common-base/src/main/java/com/bdxh/common/base/constant/WxAuthorizedConstants.java
@@ -10,17 +10,17 @@ public class WxAuthorizedConstants {
         /**
          * 公众账号ID
          */
-        String appid="wxb5d90dcdscdsfcv316";
+        String appid="wxb5d9031684f864e4";
 
         /**
          * 秘钥
          */
-        String secret="bcb39fc81escsdcdec65445dscsd2bad8f75415";
+        String secret="bcb39fc81ed870ba654452bad8f75415";
 
         /**
          * 通过code换取access_token地址
          */
-        String urlList="https://api.weixin.qq.com/sns/oa";
+        String urlList="https://api.weixin.qq.com/sns/oauth2/access_token";
 
         /**
          * 授权默认参数url
