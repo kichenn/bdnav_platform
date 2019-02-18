ba94f68ab3300b80e223e38cfc8edd4e68484b24
xuyuan <532097775@qq.com>
2019-02-18 17:49:29 +0800

    框架

--- bdnav-provider/wallet-provider/src/main/java/com/bdxh/wallet/configration/common/RestTemplateConfig.java
+++ bdnav-provider/wallet-provider/src/main/java/com/bdxh/wallet/configration/common/RestTemplateConfig.java
@@ -14,7 +14,13 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.nio.charset.Charset;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -30,9 +36,9 @@ public class RestTemplateConfig {
 
     @Bean
     @ConditionalOnMissingBean({ClientHttpRequestFactory.class})
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        log.info("simpleClientHttpRequestFactory初始化开始------------------------");
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+    public HttpsClientRequestFactory httpsClientRequestFactory() {
+        log.info("httpsClientRequestFactory初始化开始------------------------");
+        HttpsClientRequestFactory factory = new HttpsClientRequestFactory();
         factory.setReadTimeout(20000);
         factory.setConnectTimeout(5000);
         return factory;
@@ -40,9 +46,9 @@ public class RestTemplateConfig {
 
     @Bean
     @ConditionalOnMissingBean({ RestOperations.class, RestTemplate.class })
-    public RestTemplate restTemplate(@Autowired ClientHttpRequestFactory simpleClientHttpRequestFactory) {
+    public RestTemplate restTemplate(@Autowired HttpsClientRequestFactory httpsClientRequestFactory) {
         log.info("restTemplate初始化开始------------------------");
-        RestTemplate restTemplate = new RestTemplate(simpleClientHttpRequestFactory);
+        RestTemplate restTemplate = new RestTemplate(httpsClientRequestFactory);
         List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
         Iterator<HttpMessageConverter<?>> iterator = messageConverters.iterator();
         while (iterator.hasNext()) {
@@ -64,6 +70,105 @@ public class RestTemplateConfig {
             mediaTypes.add(MediaType.TEXT_PLAIN);
             setSupportedMediaTypes(mediaTypes);
         }
+    }
+
+
+    public class HttpsClientRequestFactory extends SimpleClientHttpRequestFactory {
+
+        @Override
+        protected void prepareConnection(HttpURLConnection connection, String httpMethod) {
+            try {
+                if (!(connection instanceof HttpsURLConnection)) {
+                    throw new RuntimeException("An instance of HttpsURLConnection is expected");
+                }
+                HttpsURLConnection httpsConnection = (HttpsURLConnection) connection;
+                TrustManager[] trustAllCerts = new TrustManager[]{
+                        new X509TrustManager() {
+                            @Override
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return null;
+                            }
+                            @Override
+                            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                            }
+                            @Override
+                            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                            }
+                        }
+                };
+                SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+                httpsConnection.setSSLSocketFactory(new MyCustomSSLSocketFactory(sslContext.getSocketFactory()));
+                httpsConnection.setHostnameVerifier((str,sslSession)->true);
+                super.prepareConnection(httpsConnection, httpMethod);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public class MyCustomSSLSocketFactory extends SSLSocketFactory {
+
+        private final SSLSocketFactory delegate;
+
+        public MyCustomSSLSocketFactory(SSLSocketFactory delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return delegate.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return delegate.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket(final Socket socket, final String host, final int port,
+                                   final boolean autoClose) throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(socket, host, port, autoClose);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final String host, final int port) throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final String host, final int port, final InetAddress localAddress,
+                                   final int localPort) throws
+                IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port, localAddress, localPort);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final InetAddress host, final int port) throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final InetAddress host, final int port, final InetAddress localAddress,
+                                   final int localPort) throws
+                IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port, localAddress, localPort);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        private Socket overrideProtocol(final Socket socket) {
+            if (!(socket instanceof SSLSocket)) {
+                throw new RuntimeException("An instance of SSLSocket is expected");
+            }
+            ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLSv1"});
+            return socket;
+        }
+
     }
 
 }
