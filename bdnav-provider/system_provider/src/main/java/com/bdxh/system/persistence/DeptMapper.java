5bf49b3d410287130e8035d549f16ec412db245e
litao <762118752@qq.com>
2019-02-22 16:40:06 +0800

    system模块

---
+++ bdnav-provider/system_provider/src/main/java/com/bdxh/system/persistence/DeptMapper.java
@@ -0,0 +1,7 @@
+package com.bdxh.system.persistence;
+
+import com.bdxh.system.entity.Dept;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface DeptMapper extends Mapper<Dept> {
+}
---
+++ bdnav-provider/system_provider/src/main/java/com/bdxh/system/persistence/UserMapper.java
@@ -0,0 +1,7 @@
+package com.bdxh.system.persistence;
+
+import com.bdxh.system.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
