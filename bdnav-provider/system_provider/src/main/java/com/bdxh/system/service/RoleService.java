f07cd4e96f05f45cee79894cd8a75e8e5ff30f84
litao <762118752@qq.com>
2019-02-26 16:19:03 +0800

    system系统模块编写

--- bdnav-provider/system_provider/src/main/java/com/bdxh/system/service/RoleService.java
+++ bdnav-provider/system_provider/src/main/java/com/bdxh/system/service/RoleService.java
@@ -2,8 +2,10 @@ package com.bdxh.system.service;
 
 import com.bdxh.common.web.support.IService;
 import com.bdxh.system.entity.Role;
+import com.github.pagehelper.PageInfo;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @description: 角色管理service
@@ -15,5 +17,10 @@ public interface RoleService extends IService<Role> {
     void delRole(Long roleId);
 
     void delBatchRole(List<Long> roleIds);
+
+    List<Role> findList(Map<String,Object> param);
+
+    PageInfo<Role> findRoleList(Map<String,Object> param, int pageNum, int pageSize);
+
 
 }
