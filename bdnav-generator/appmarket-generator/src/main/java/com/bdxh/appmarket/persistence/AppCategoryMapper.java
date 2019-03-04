35e231743bda690e8c413543b270b1c2fc6198d9
xuyuan <532097775@qq.com>
2019-03-04 17:07:01 +0800

    app

---
+++ bdnav-generator/appmarket-generator/src/main/java/com/bdxh/appmarket/persistence/AppCategoryMapper.java
@@ -0,0 +1,7 @@
+package com.bdxh.appmarket.persistence;
+
+import com.bdxh.appmarket.entity.AppCategory;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AppCategoryMapper extends Mapper<AppCategory> {
+}
