cf9b8c00156f223bde347eddfe2cf1eba70e5028
xuyuan <532097775@qq.com>
2019-03-11 19:38:01 +0800

    app

--- bdnav-web/app-web/src/main/java/com/bdxh/app/configration/security/OAuth2ServerConfig.java
+++ bdnav-web/app-web/src/main/java/com/bdxh/app/configration/security/OAuth2ServerConfig.java
@@ -3,6 +3,7 @@ package com.bdxh.app.configration.security;
 import com.bdxh.app.configration.security.handler.MyExceptionTranslator;
 import com.bdxh.app.configration.security.handler.MyAccessDeniedHandler;
 import com.bdxh.app.configration.security.handler.MyUnauthorizedHandler;
+import com.bdxh.app.configration.security.properties.SecurityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
@@ -31,7 +34,17 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 @EnableAuthorizationServer
 public class OAuth2ServerConfig {
 
+    //获取token
     //http://localhost:9027/oauth/token?username=user_1&password=123456&grant_type=password&client_id=password&client_secret=123456
+
+    //刷新token
+    //http://localhost:9027/oauth/token?client_id=password&client_secret=123456&grant_type=refresh_token&refresh_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiYXBwLXdlYiJdLCJkYXRhIjoi5rWL6K-V5pWw5o2uIiwidXNlcl9uYW1lIjoieHV5dWFuIiwic2NvcGUiOlsiYWxsIl0sImF0aSI6IjJmZTMwY2RkLTYwZGYtNGViNi1hNzUyLTRkYWUxYjc0MDZlOSIsImV4cCI6MTU1NDg3NzgyNSwianRpIjoiZDllNTI1MDMtNThjZC00NTA4LTkyYjgtYTdlOWZjMjM5MThmIiwiY2xpZW50X2lkIjoicGFzc3dvcmQifQ.KEx7sOlKNiGh7mXItON6H-_ll-D79nZ4hu7u8YAGKrarrIUEPBKLQ9LvPxggCKqwZJhnBUZmwwc7XOdk66Drs20InaT6AtFnez7DqnrB5_8AA4dCBv0m9eIc-hAqj9ClcH5bhl0TyrIMlAL2-zE9aKftfeEJx9xnyf6FoEft61l4EQ1B1OdLJEuHUN6IsXZRYztemT94UugJGn30vO03-QVNNRx_iHtjGMf8n7LuvRiWvs6kSFactQjQRtpJGt7zcPu2gbNTtr1bpaHtdt93XcIqapGy4NnU5wbUtZYEDGZG5R9ECuIvPej79PInXvaio_FpaGffxoZVdZOAPUVT_w
+
+    //验证token
+    //http://localhost:9027/oauth/check_token?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiYXBwLXdlYiJdLCJkYXRhIjoi5rWL6K-V5pWw5o2uIiwidXNlcl9uYW1lIjoieHV5dWFuIiwic2NvcGUiOlsiYWxsIl0sImV4cCI6MTU1MjM5MDQ2NSwianRpIjoiYzY2NjA0ZTQtNjkxYy00YjliLWE5NjctODI1OGFhNjFjNmZiIiwiY2xpZW50X2lkIjoicGFzc3dvcmQifQ.hZsLWzq82F5I4bo_Y_NQTaRMvi8M8N7ZYecLEqmmBGxbgGeSky6VxTx9TpBDg_qZ0yZMwAffknE6JatTG9Wxz0zmD97Zbnk_NsoNZl2U7fLNOh8QLee-Oj-uBHuMuP0b_l9LIQJunkNV03B8Uiy7G_BYe8PWJKx7BWd1WVuUFJI08-v14VBcxesOBXqjpuutB9lmJqnKkfJZK2GqoN5OppEEa9O48jLMlofZ7J8vRZ60aAQ7UleypLGXrHYL0TZJYPsdqYUi-yWBTruE5-He2BGXhhDiz7VHwRgapZF4SMZ40PxXThqxTlzZlW1L2kTzQl2yOtghunEWcDkI7ZewoA
+
+    //获取公钥
+    //http://localhost:9027/oauth/token_key
 
     private static final String APP_WEB_RESOURCE_ID = "app-web";
 
@@ -74,6 +87,12 @@ public class OAuth2ServerConfig {
     protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
         @Autowired
+        private MyUnauthorizedHandler myUnauthorizedHandler;
+
+        @Autowired
+        private MyAccessDeniedHandler myAccessDeniedHandler;
+
+        @Autowired
         private MyExceptionTranslator myExceptionTranslator;
 
         @Autowired
@@ -113,11 +132,23 @@ public class OAuth2ServerConfig {
                     .exceptionTranslator(myExceptionTranslator)
                     .authenticationManager(authenticationManager)
                     .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+            DefaultTokenServices tokenServices = (DefaultTokenServices) endpoints.getDefaultAuthorizationServerTokenServices();
+            tokenServices.setTokenStore(endpoints.getTokenStore());
+            tokenServices.setSupportRefreshToken(true);
+            tokenServices.setReuseRefreshToken(true);
+            tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+            tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+            tokenServices.setAccessTokenValiditySeconds(SecurityConstant.TOKEN_VALIDITY_SECONDS);
+            tokenServices.setRefreshTokenValiditySeconds(SecurityConstant.REFRESH_TOKEN_VALIDITY_SECONDS);
         }
 
         @Override
         public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
-            oauthServer.allowFormAuthenticationForClients();
+            oauthServer.authenticationEntryPoint(myUnauthorizedHandler)
+                    .accessDeniedHandler(myAccessDeniedHandler)
+                    .checkTokenAccess("permitAll()")
+                    .tokenKeyAccess("permitAll()")
+                    .allowFormAuthenticationForClients();
         }
 
     }
