a144fb233f08a4fa6e6a47ce636421a4a304d4f2
likang <jaywilder@163.com>
2019-03-27 18:10:11 +0800

    增加门禁信息

---
+++ bdnav-provider-entity/school-provider-entity/src/main/java/com/bdxh/school/dto/AddSchoolDeviceDto.java
@@ -0,0 +1,55 @@
+package com.bdxh.school.dto;
+
+import com.bdxh.school.enums.DeviceStatusEnum;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @Description: 增加门禁信息dto
+ * @Author: Kang
+ * @Date: 2019/3/27 17:43
+ */
+@Data
+public class AddSchoolDeviceDto {
+
+    @ApiModelProperty("学校id")
+    private Long schoolId;
+
+    @ApiModelProperty("学校编码")
+    private String schoolCode;
+
+    @ApiModelProperty("设备编码")
+    private String deviceId;
+
+    @ApiModelProperty("设备类型")
+    private Byte deviceType;
+
+    @ApiModelProperty("设备类型名称")
+    private String deviceTypeName;
+
+    @ApiModelProperty("设备品牌")
+    private String deviceBrand;
+
+    @ApiModelProperty("设备型号")
+    private String deviceModel;
+
+    @ApiModelProperty("设备名称")
+    private String deviceName;
+
+    @ApiModelProperty("设备地址")
+    private String deviceAddress;
+
+    @ApiModelProperty("ip")
+    private String deviceIp;
+
+    @ApiModelProperty("设备状态 1 正常 2 离线")
+    private DeviceStatusEnum deviceStatusEnum;
+
+    @ApiModelProperty("注释")
+    private String remark;
+
+
+}
---
+++ bdnav-provider-entity/school-provider-entity/src/main/java/com/bdxh/school/dto/ModifyGroupPermissionDto.java
@@ -0,0 +1,67 @@
+package com.bdxh.school.dto;
+
+import com.bdxh.school.enums.AccessFlagEnum;
+import com.bdxh.school.enums.GroupTypeEnum;
+import com.bdxh.school.enums.RecursionPermissionStatusEnum;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @Description: 修改门禁组 dto
+ * @Author: Kang
+ * @Date: 2019/3/27 17:42
+ */
+@Data
+public class ModifyGroupPermissionDto {
+
+
+    @NotNull
+    @ApiModelProperty("id")
+    private Long id;
+
+    @ApiModelProperty("学校id")
+    private Long schoolId;
+
+    @ApiModelProperty("学校编码")
+    private String schoolCode;
+
+    @ApiModelProperty("用户群类型 1 学生 2 老师")
+    private GroupTypeEnum groupTypeEnum;
+
+    @ApiModelProperty("部门id")
+    private Long groupId;
+
+    @ApiModelProperty("是否递归权限 1 是 2 否")
+    private RecursionPermissionStatusEnum recursionPermissionStatusEnum;
+
+    @ApiModelProperty("递归权限ids")
+    private String recursionPermissionIds;
+
+    @ApiModelProperty("设备id")
+    private Long deviceId;
+
+    @ApiModelProperty("设备编码")
+    private String deviceCode;
+
+    @ApiModelProperty("权限标识 1 允许 2 不允许")
+    private AccessFlagEnum accessFlagEnum;
+
+    @ApiModelProperty("允许开始时间")
+    private Date accessBegin;
+
+    @ApiModelProperty("允许结束时间")
+    private Date accessEnd;
+
+    @ApiModelProperty("周时间段")
+    private String accessDayMark;
+
+    @ApiModelProperty("日时间段 00111(这种形式0允许1不允许)")
+    private String accessTimeMark;
+
+    @ApiModelProperty("备注")
+    private String remark;
+
+}
