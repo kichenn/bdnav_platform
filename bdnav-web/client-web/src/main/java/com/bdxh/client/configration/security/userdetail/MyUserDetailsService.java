fdb5199081f36aac6690a8f803f51be99edebad5
xuyuan <532097775@qq.com>
2019-04-08 10:26:00 +0800

    改动

--- bdnav-web/client-web/src/main/java/com/bdxh/client/configration/security/userdetail/MyUserDetailsService.java
+++ bdnav-web/client-web/src/main/java/com/bdxh/client/configration/security/userdetail/MyUserDetailsService.java
@@ -1,10 +1,10 @@
 package com.bdxh.client.configration.security.userdetail;
 
 import com.bdxh.common.utils.wrapper.Wrapper;
-import com.bdxh.system.entity.User;
-import com.bdxh.system.feign.PermissionControllerClient;
-import com.bdxh.system.feign.RoleControllerClient;
-import com.bdxh.system.feign.UserControllerClient;
+import com.bdxh.school.entity.SchoolUser;
+import com.bdxh.school.feign.SchoolPermissionControllerClient;
+import com.bdxh.school.feign.SchoolRoleControllerClient;
+import com.bdxh.school.feign.SchoolUserControllerClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,31 +24,31 @@ import java.util.List;
 public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private UserControllerClient userControllerClient;
+    private SchoolUserControllerClient schoolUserControllerClient;
 
     @Autowired
-    private RoleControllerClient roleControllerClient;
+    private SchoolRoleControllerClient schoolRoleControllerClient;
 
     @Autowired
-    private PermissionControllerClient permissionControllerClient;
+    private SchoolPermissionControllerClient schoolPermissionControllerClient;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //根据用户名查询用户
-        Wrapper<User> userWrapper = userControllerClient.queryUserByUserName(username);
-        User user = userWrapper.getResult();
+        Wrapper<SchoolUser> userWrapper = schoolUserControllerClient.findSchoolUserByName(username);
+        SchoolUser user = userWrapper.getResult();
         if (user == null){
             throw new UsernameNotFoundException("用户不存在");
         }
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         //根据用户id查询角色列表
-        Wrapper<List<String>> roleWrapper = roleControllerClient.queryRoleListByUserId(user.getId());
+        Wrapper<List<String>> roleWrapper = schoolRoleControllerClient.findSchoolRolesByUserId(user.getId());
         List<String> roles = roleWrapper.getResult();
         if (roles != null && !roles.isEmpty()){
             roles.forEach(role->authorities.add(new SimpleGrantedAuthority(role)));
         }
         //根据用户id查询权限列表
-        Wrapper<List<String>> permissionWrapper = permissionControllerClient.permissionMenus(user.getId());
+        Wrapper<List<String>> permissionWrapper = schoolPermissionControllerClient.();
         List<String> permissions = permissionWrapper.getResult();
         if (permissions!=null && !permissions.isEmpty()){
             permissions.forEach(permission->authorities.add(new SimpleGrantedAuthority(permission)));
