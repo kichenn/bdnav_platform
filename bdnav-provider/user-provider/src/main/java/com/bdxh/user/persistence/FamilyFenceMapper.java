a2eff55b6518b7f62c16f8847293360d619af5fc
binzhuohao <724323488@qq.com>
2019-04-11 18:08:21 +0800

    学校端的对接修改

--- bdnav-provider/user-provider/src/main/java/com/bdxh/user/persistence/FamilyFenceMapper.java
+++ bdnav-provider/user-provider/src/main/java/com/bdxh/user/persistence/FamilyFenceMapper.java
@@ -9,36 +9,17 @@ import java.util.List;
 
 @Repository
 public interface FamilyFenceMapper extends Mapper<FamilyFence> {
-    /**
-     * 修改围栏表信息
-     * @param familyFence
-     * @return
-     */
+
+    //修改围栏表信息
     int updateFamilyFenceInfo(@Param("familyFence")FamilyFence familyFence);
 
-    /**
-     * 删除围栏表信息
-     * @param schoolCode
-     * @param cardNumber
-     * @param id
-     * @return
-     */
+    //删除围栏表信息
     int removeFamilyFenceInfo(@Param("schoolCode")String schoolCode,@Param("cardNumber")String cardNumber,@Param("id")String id);
 
-    /**
-     * 获取围栏表所有信息
-     * @param familyFence
-     * @return
-     */
+    // 获取围栏表所有信息
     List<FamilyFence> getFamilyFenceInfos(@Param("familyFence") FamilyFence familyFence);
 
-    /**
-     * 获取围栏表单个信息
-     * @param schoolCode
-     * @param cardNumber
-     * @param id
-     * @return
-     */
+    //获取围栏表单个信息
     FamilyFence getFamilyFenceInfo(@Param("schoolCode")String schoolCode,@Param("cardNumber")String cardNumber,@Param("id")String id);
 
 }
