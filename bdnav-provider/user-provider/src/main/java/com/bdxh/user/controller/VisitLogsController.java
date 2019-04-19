7b5e64d06515b59810424ede6c299aa8692cac77
binzhuohao <724323488@qq.com>
2019-04-19 16:48:45 +0800

    围栏警报和学生访问日志全部改成存入mongoDB

--- bdnav-provider/user-provider/src/main/java/com/bdxh/user/controller/VisitLogsController.java
+++ bdnav-provider/user-provider/src/main/java/com/bdxh/user/controller/VisitLogsController.java
@@ -13,10 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -27,7 +24,7 @@ import java.util.stream.Collectors;
 * @Author Kang
 * @Date 2019-04-17 17:29:24
 */
-@Controller
+@RestController
 @RequestMapping("/visitLogs")
 @Slf4j
 @Validated
