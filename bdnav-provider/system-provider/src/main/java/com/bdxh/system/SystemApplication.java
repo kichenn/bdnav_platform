3073f7badc1da3d16047ab0cc733060125550645
litao <762118752@qq.com>
2019-04-23 19:31:24 +0800

    修改配置文件

--- bdnav-provider/system-provider/src/main/java/com/bdxh/system/SystemApplication.java
+++ bdnav-provider/system-provider/src/main/java/com/bdxh/system/SystemApplication.java
@@ -1,5 +1,6 @@
 package com.bdxh.system;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -8,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @ServletComponentScan
 @EnableEurekaClient
+@EnableApolloConfig
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);
