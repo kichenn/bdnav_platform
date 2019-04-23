0144659ba6c475e85fb1e5aa51cee97391fb5760
binzhuohao <724323488@qq.com>
2019-04-23 18:49:40 +0800

    改成apollo配置中心

--- bdnav-provider/appburied-provider/src/main/java/com/bdxh/appburied/AppburiedApplication.java
+++ bdnav-provider/appburied-provider/src/main/java/com/bdxh/appburied/AppburiedApplication.java
@@ -1,5 +1,6 @@
 package com.bdxh.appburied;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @ServletComponentScan
 @EnableEurekaClient
+@EnableApolloConfig
 public class AppburiedApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppburiedApplication.class,args);
