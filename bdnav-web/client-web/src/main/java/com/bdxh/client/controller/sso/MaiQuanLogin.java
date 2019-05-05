a27ab33bdf425e3cab9ff27956d02ea690496bfc
binzhuohao <724323488@qq.com>
2019-05-05 12:02:51 +0800

    添加了登录麦圈所需要的参数接口

--- bdnav-web/client-web/src/main/java/com/bdxh/client/controller/sso/MaiQuanLogin.java
+++ bdnav-web/client-web/src/main/java/com/bdxh/client/controller/sso/MaiQuanLogin.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaiQuanLogin {
     @Autowired
     private SchoolUserControllerClient schoolUserControllerClient;
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
     public Object login(){
         SchoolUser schoolUser=SecurityUtils.getCurrentUser();
         JSONObject jsonObject=new JSONObject();
