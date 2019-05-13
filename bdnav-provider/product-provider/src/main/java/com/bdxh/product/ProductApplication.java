ac4ead1d91942dc2da8a952289a96d8003d3e4b6
binzhuohao <724323488@qq.com>
2019-05-13 09:50:13 +0800

    修改

--- bdnav-provider/product-provider/src/main/java/com/bdxh/product/ProductApplication.java
+++ bdnav-provider/product-provider/src/main/java/com/bdxh/product/ProductApplication.java
@@ -3,6 +3,7 @@ package com.bdxh.product;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @create: 2019-01-19 16:19
  **/
 @SpringBootApplication
+@ServletComponentScan
 @EnableEurekaClient
 @EnableApolloConfig
 public class ProductApplication {
