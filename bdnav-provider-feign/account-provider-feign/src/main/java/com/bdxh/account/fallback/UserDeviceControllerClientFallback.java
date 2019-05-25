98559f0a93cfe833f84c84b87c6d6fe43212517b
litao <762118752@qq.com>
2019-05-25 18:42:22 +0800

    完善用户登录设备信息

--- bdnav-provider-feign/account-provider-feign/src/main/java/com/bdxh/account/fallback/UserDeviceControllerClientFallback.java
+++ bdnav-provider-feign/account-provider-feign/src/main/java/com/bdxh/account/fallback/UserDeviceControllerClientFallback.java
@@ -1,9 +1,13 @@
 package com.bdxh.account.fallback;
 
+import com.bdxh.account.dto.AddUserDevice;
+import com.bdxh.account.dto.ModifyUserDevice;
 import com.bdxh.account.feign.UserDeviceControllerClient;
 import com.bdxh.common.utils.wrapper.WrapMapper;
 import com.bdxh.common.utils.wrapper.Wrapper;
 import org.springframework.stereotype.Component;
+
+import javax.validation.Valid;
 
 @Component
 public class UserDeviceControllerClientFallback implements UserDeviceControllerClient {
@@ -11,6 +15,21 @@ public class UserDeviceControllerClientFallback implements UserDeviceControllerC
 
     @Override
     public Wrapper getUserDeviceAll(String schoolCode, Long groupId) {
+        return WrapMapper.error();
+    }
+
+    @Override
+    public Wrapper addUserDeviceInfo(@Valid AddUserDevice addUserDevice) {
+        return WrapMapper.error();
+    }
+
+    @Override
+    public Wrapper modifyUserDeviceInfo(@Valid ModifyUserDevice modifyUserDevice) {
+        return WrapMapper.error();
+    }
+
+    @Override
+    public Wrapper delUserDeviceById(@Valid Long id) {
         return WrapMapper.error();
     }
 }
