6e0da5b66051ac21a8c9d308e2f28d887c10de97
litao <762118752@qq.com>
2019-05-30 09:58:35 +0800

    完善查询预置应用

--- bdnav-web/app-web/src/main/java/com/bdxh/app/controller/applycontrols/ApplyControlsWebController.java
+++ bdnav-web/app-web/src/main/java/com/bdxh/app/controller/applycontrols/ApplyControlsWebController.java
@@ -17,14 +17,12 @@ import com.bdxh.common.utils.wrapper.WrapMapper;
 import com.bdxh.common.utils.wrapper.Wrapper;
 import com.bdxh.school.dto.BlackUrlQueryDto;
 import com.bdxh.school.feign.BlackUrlControllerClient;
-import com.bdxh.school.feign.SchoolStrategyControllerClient;
 import com.bdxh.user.dto.UpdateStudentDto;
 import com.bdxh.user.feign.StudentControllerClient;
 import com.bdxh.user.vo.StudentVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -38,7 +36,6 @@ import java.util.Map;
  * 应用管控API
  */
 @RestController
-@RequestMapping("/applyControlsWeb")
 @Slf4j
 @Validated
 @Api(value = "应用管控API信息", tags = "应用管控交互API")
@@ -65,37 +62,37 @@ public class ApplyControlsWebController {
 
 
     @ApiOperation(value = "修改学生个人信息", response = Boolean.class)
-    @RequestMapping(value = "/modifyInfo", method = RequestMethod.POST)
+    @RequestMapping(value = "/applyControlsWeb/modifyInfo", method = RequestMethod.POST)
     public Object modifyInfo(@Validated @RequestBody UpdateStudentDto updateStudentDto) {
         return studentControllerClient.updateStudent(updateStudentDto);
     }
 
     @ApiOperation(value = "显示学生信息详情", response = Boolean.class)
-    @RequestMapping(value = "/infoDetails", method = RequestMethod.GET)
+    @RequestMapping(value = "/applyControlsWeb/infoDetails", method = RequestMethod.GET)
     public Object infoDetails(@Validated @RequestParam(name = "schoolCode") String schoolCode, @RequestParam(name = "cardNumber") String cardNumber) {
         return studentControllerClient.queryStudentInfo(schoolCode, cardNumber);
     }
 
     @ApiOperation(value = "本地应用上报接口", response = Boolean.class)
-    @RequestMapping(value = "/ReportApp", method = RequestMethod.POST)
+    @RequestMapping(value = "/applyControlsWeb/ReportApp", method = RequestMethod.POST)
     public Object ReportApp(@Validated @RequestBody AddInstallAppsDto addInstallAppsDto) {
         return installAppsControllerClient.addInstallApp(addInstallAppsDto);
     }
 
     @ApiOperation(value = "学校黑名单", response = Boolean.class)
-    @RequestMapping(value = "/blackList", method = RequestMethod.POST)
+    @RequestMapping(value = "/applyControlsWeb/blackList", method = RequestMethod.POST)
     public Object blackList(@Validated @RequestBody BlackUrlQueryDto blackUrlQueryDto) {
         return blackUrlControllerClient.findBlackInConditionPaging(blackUrlQueryDto);
     }
 
     @ApiOperation(value = "查询用户被禁名单列表", response = Boolean.class)
-    @RequestMapping(value = "/disableAppList", method = RequestMethod.POST)
+    @RequestMapping(value = "/applyControlsWeb/disableAppList", method = RequestMethod.POST)
     public Object disableAppList(@Validated @RequestBody AppStatusQueryDto appStatusQueryDto) {
         return appStatusControllerClient.findAppStatusInContionPaging(appStatusQueryDto);
     }
 
     @ApiOperation(value = "最新应用版本查询", response = Boolean.class)
-    @RequestMapping(value = "/versionUpdating", method = RequestMethod.GET)
+    @RequestMapping(value = "/applyControlsWeb/versionUpdating", method = RequestMethod.GET)
     public Object versionUpdating(@RequestParam("appId") Long appId) {
         Wrapper<AppVersion> wrapper = appVersionControllerClient.findNewAppVersion(appId);
         return WrapMapper.ok(wrapper.getResult());
@@ -104,7 +101,7 @@ public class ApplyControlsWebController {
 
 
     @ApiOperation(value = "查询预置应用列表", response = Boolean.class)
-    @RequestMapping(value = "/thePresetList", method = RequestMethod.POST)
+    @RequestMapping(value = "/authenticationApp/thePresetList", method = RequestMethod.POST)
     public Object thePresetList() {
         Wrapper wrapper = appControllerClient.thePresetList();
         return WrapMapper.ok(wrapper.getResult());
@@ -113,7 +110,7 @@ public class ApplyControlsWebController {
 
 
     @ApiOperation(value = "修改个人头像", response = Boolean.class)
-    @RequestMapping(value = "/modifyInfoPhoto", method = RequestMethod.POST)
+    @RequestMapping(value = "/applyControlsWeb/modifyInfoPhoto", method = RequestMethod.POST)
     public Object modifyInfoPhoto(MultipartFile multipartFile) {
         //获取账户信息
         Account account = SecurityUtils.getCurrentUser();
@@ -145,14 +142,14 @@ public class ApplyControlsWebController {
     }
 
     @ApiOperation(value = "批量上报应用信息", response = Boolean.class)
-    @RequestMapping(value = "/batchSaveInstallAppsInfo", method = RequestMethod.POST)
+    @RequestMapping(value = "/applyControlsWeb/batchSaveInstallAppsInfo", method = RequestMethod.POST)
     public Object batchSaveInstallAppsInfo(@RequestBody List<AddInstallAppsDto> appInstallList) {
         Wrapper wrapper = installAppsControllerClient.batchSaveInstallAppsInfo(appInstallList);
         return WrapMapper.ok(wrapper.getResult());
     }
 
     @ApiOperation(value = "删除上报应用信息", response = Boolean.class)
-    @RequestMapping(value = "/delInstallAppById", method = RequestMethod.POST)
+    @RequestMapping(value = "/applyControlsWeb/delInstallAppById", method = RequestMethod.POST)
     public Object delInstallAppById(@RequestBody DelOrFindAppBuriedDto delInstallAppsDto) {
         Wrapper wrapper = installAppsControllerClient.delInstallAppById(delInstallAppsDto);
         return WrapMapper.ok(wrapper.getResult());
