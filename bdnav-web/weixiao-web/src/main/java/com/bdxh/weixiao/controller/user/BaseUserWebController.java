412974020898cd71890ad3024848f50e8fe26157
binzhuohao <724323488@qq.com>
2019-06-13 15:01:41 +0800

    测试激活

--- bdnav-web/weixiao-web/src/main/java/com/bdxh/weixiao/controller/user/BaseUserWebController.java
+++ bdnav-web/weixiao-web/src/main/java/com/bdxh/weixiao/controller/user/BaseUserWebController.java
@@ -52,10 +52,10 @@ public class BaseUserWebController {
     @RequestMapping(value = "/activationBaseUser", method = RequestMethod.POST)
     public Object activationBaseUser(@RequestBody ActivationBaseUserDto activationBaseUserDto) {
            //判断手机验证码是否正确
-            String saveCode=redisUtil.get(AliyunSmsConstants.CodeConstants.CAPTCHA_PREFIX +activationBaseUserDto.getPhone());
+/*            String saveCode=redisUtil.get(AliyunSmsConstants.CodeConstants.CAPTCHA_PREFIX +activationBaseUserDto.getPhone());
             if(!activationBaseUserDto.getCode().equals(saveCode)){
                 return WrapMapper.error("手机验证码错误");
-            }
+            }*/
             School school = schoolControllerClient.findSchoolBySchoolCode(activationBaseUserDto.getSchoolCode()).getResult();
             activationBaseUserDto.setAppKey(school.getAppKey());
             activationBaseUserDto.setAppSecret(school.getAppSecret());
