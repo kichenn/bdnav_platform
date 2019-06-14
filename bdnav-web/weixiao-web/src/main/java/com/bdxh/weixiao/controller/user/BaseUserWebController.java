1af2f4f279a2f0d199b0b3d4c021a5a9400f08c0
binzhuohao <724323488@qq.com>
2019-06-14 11:28:47 +0800

    测试激活

--- bdnav-web/weixiao-web/src/main/java/com/bdxh/weixiao/controller/user/BaseUserWebController.java
+++ bdnav-web/weixiao-web/src/main/java/com/bdxh/weixiao/controller/user/BaseUserWebController.java
@@ -29,7 +29,6 @@ import javax.validation.constraints.NotNull;
  * @create: 2019-04-26 14:06
  **/
 @Slf4j
-@RequestMapping(value = "/authenticationWeixiao/baseUserWeb")
 @RestController
 @Api(value = "微校平台----微校学校用户(学生 ， 家长 ， 老师)控制器", tags = "微校平台----微校学校用户(学生 ， 家长 ， 老师)控制器")
 @Validated
@@ -49,13 +48,13 @@ public class BaseUserWebController {
      * @return
      */
     @ApiOperation(value = "微校平台----用户激活接口")
-    @RequestMapping(value = "/activationBaseUser", method = RequestMethod.POST)
+    @RequestMapping(value = "/authenticationWeixiao/activationBaseUser", method = RequestMethod.POST)
     public Object activationBaseUser(@RequestBody ActivationBaseUserDto activationBaseUserDto) {
            //判断手机验证码是否正确
-            String saveCode=redisUtil.get(AliyunSmsConstants.CodeConstants.CAPTCHA_PREFIX +activationBaseUserDto.getPhone());
+/*            String saveCode=redisUtil.get(AliyunSmsConstants.CodeConstants.CAPTCHA_PREFIX +activationBaseUserDto.getPhone());
             if(!activationBaseUserDto.getCode().equals(saveCode)){
                 return WrapMapper.error("手机验证码错误");
-            }
+            }*/
             School school = schoolControllerClient.findSchoolBySchoolCode(activationBaseUserDto.getSchoolCode()).getResult();
             activationBaseUserDto.setAppKey(school.getSchoolKey());
             activationBaseUserDto.setAppSecret(school.getSchoolSecret());
@@ -69,7 +68,7 @@ public class BaseUserWebController {
      * @return
      */
     @ApiOperation(value = "微校平台----激活校园卡时手机获取短信验证码")
-    @RequestMapping(value = "/getPhoneCode",method = RequestMethod.POST)
+    @RequestMapping(value = "/authenticationWeixiao/getPhoneCode",method = RequestMethod.POST)
     public Object getPhoneCode(@RequestParam(name="phone")@NotNull(message = "手机号码不能为空") String phone){
         return baseUserControllerClient.getPhoneCode(phone);
     }
