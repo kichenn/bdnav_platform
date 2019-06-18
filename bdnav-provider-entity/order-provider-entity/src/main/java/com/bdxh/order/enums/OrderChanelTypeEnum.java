4d02a89fd6e08991131fe80a1cefefead873a4e4
likang <jaywilder@163.com>
2019-06-18 15:03:29 +0800

    commit 微信支付订单相关

--- bdnav-provider-entity/order-provider-entity/src/main/java/com/bdxh/order/enums/OrderChanelTypeEnum.java
+++ bdnav-provider-entity/order-provider-entity/src/main/java/com/bdxh/order/enums/OrderChanelTypeEnum.java
@@ -1,21 +1,21 @@
 package com.bdxh.order.enums;
 
 /**
- * @description: 微信支付状态枚举类
- * @author: xuyuan
- * @create: 2019-01-04 11:02
- **/
+ * @Description: 微信支付状态枚举类
+ * @Author: Kang
+ * @Date: 2019/6/18 14:24
+ */
 public enum OrderChanelTypeEnum {
 
-    BDXH_SELF(new Byte("1"),"自有渠道");
+    BDXH_SELF(new Byte("1"), "自有渠道");
 
     private Byte code;
 
     private String desc;
 
-    OrderChanelTypeEnum(Byte code, String desc){
-        this.code=code;
-        this.desc=desc;
+    OrderChanelTypeEnum(Byte code, String desc) {
+        this.code = code;
+        this.desc = desc;
     }
 
     public Byte getCode() {
--- bdnav-provider-entity/order-provider-entity/src/main/java/com/bdxh/order/enums/OrderPayStatusEnum.java
+++ bdnav-provider-entity/order-provider-entity/src/main/java/com/bdxh/order/enums/OrderPayStatusEnum.java
@@ -1,24 +1,24 @@
 package com.bdxh.order.enums;
 
 /**
- * @description: 订单支付状态枚举类
- * @author: xuyuan
- * @create: 2019-01-09 16:26
- **/
+ * @Description: 订单支付状态枚举类
+ * @Author: Kang
+ * @Date: 2019/6/18 14:24
+ */
 public enum OrderPayStatusEnum {
 
-    NO_PAY(new Byte("1"),"未支付"),
-    PAYING(new Byte("2"),"支付中"),
-    PAY_FAIL(new Byte("3"),"支付失败"),
-    PAY_SUCCESS(new Byte("4"),"支付成功");
+    NO_PAY(new Byte("1"), "未支付"),
+    PAYING(new Byte("2"), "支付中"),
+    PAY_FAIL(new Byte("3"), "支付失败"),
+    PAY_SUCCESS(new Byte("4"), "支付成功");
 
     private Byte code;
 
     private String desc;
 
-    OrderPayStatusEnum(Byte code, String desc){
-        this.code=code;
-        this.desc=desc;
+    OrderPayStatusEnum(Byte code, String desc) {
+        this.code = code;
+        this.desc = desc;
     }
 
     public Byte getCode() {
