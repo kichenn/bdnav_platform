a9f0fe9bfb2881eda490480003a28c5e66748388
likang <jaywilder@163.com>
2019-06-19 17:26:43 +0800

    commit 修改微信返参相关信息

---
+++ bdnav-common/common-utils/src/main/java/com/bdxh/common/wechatpay/js/domain/JsOrderPayResponse.java
@@ -0,0 +1,31 @@
+package com.bdxh.common.wechatpay.js.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Description: 前端H5调起支付 返参
+ * @Author: Kang
+ * @Date: 2019/6/19 17:16
+ */
+@Data
+public class JsOrderPayResponse {
+
+    @ApiModelProperty("公众号id")
+    private String appId;
+
+    @ApiModelProperty("时间戳")
+    private String timeStamp;
+
+    @ApiModelProperty("随机字符串")
+    private String nonceStr;
+
+    @ApiModelProperty("wx预订单")
+    private String prepayId;
+
+    @ApiModelProperty("签名方式")
+    private String signType="MD5";
+
+    @ApiModelProperty("签名")
+    private String paySign;
+}
