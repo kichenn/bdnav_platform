01a195351aba8f4760e45e5c957d3a7d6cca55f7
wangjiaping <123456>
2019-06-26 11:35:07 +0800

    product添加的video_url字段

--- bdnav-provider-entity/product-provider-entity/src/main/java/com/bdxh/product/dto/ProductAddDto.java
+++ bdnav-provider-entity/product-provider-entity/src/main/java/com/bdxh/product/dto/ProductAddDto.java
@@ -69,6 +69,12 @@ public class ProductAddDto implements Serializable {
     private String imgUrl;
 
     /**
+     * 视频地址
+     */
+    @ApiModelProperty("视频地址")
+    private String videoUrl;
+
+    /**
      * 业务类型 1 微校服务
      */
     @NotNull(message = "商品业务类型不能为空")
