61d95803291c612459dcdc6e387e3883afc5a625
likang <jaywilder@163.com>
2019-07-03 14:44:57 +0800

    1

--- bdnav-provider-entity/product-provider-entity/src/main/java/com/bdxh/product/dto/ProductUpdateDto.java
+++ bdnav-provider-entity/product-provider-entity/src/main/java/com/bdxh/product/dto/ProductUpdateDto.java
@@ -17,83 +17,45 @@ public class ProductUpdateDto implements Serializable {
     @ApiModelProperty("商品主键")
     private Long id;
 
-    /**
-     * 商品名称
-     */
     @ApiModelProperty("商品名称")
     private String productName;
 
-    /**
-     * 商品展示名称
-     */
     @ApiModelProperty("商品展示名称")
     private String productShowName;
 
-    /**
-     * 商品定价
-     */
     @ApiModelProperty("商品定价")
     private BigDecimal productPrice;
 
-    /**
-     * 商品售价
-     */
     @ApiModelProperty("商品售价")
     private BigDecimal productSellPrice;
 
-    /**
-     * 商品描述
-     */
     @ApiModelProperty("商品描述")
     private String productDescription;
 
-    /**
-     * 商品类型 1 单品 2 套餐
-     */
+    @ApiModelProperty("视频地址")
+    private String videoUrl;
+
     @ApiModelProperty("商品类型 1 单品 2 套餐")
     private Byte productType;
 
-    /**
-     * 商品上下架状态 1 下架 2 上架
-     */
     @ApiModelProperty("商品上下架状态 1 下架 2 上架")
     private Byte sellStatus;
 
-    /**
-     * 商品图片地址
-     */
     @ApiModelProperty("商品图片地址")
     private String imgUrl;
 
-    /**
-     * 业务类型 1 微校服务
-     */
     @ApiModelProperty("业务类型 1 微校服务")
     private Byte businessType;
 
-    /**
-     * 操作人
-     */
     @ApiModelProperty("操作人")
     private Long operator;
 
-    /**
-     * 操作姓名
-     */
     @ApiModelProperty("操作姓名")
     private String operatorName;
 
-
-    /**
-     * 备注
-     */
     @ApiModelProperty("备注")
     private String remark;
 
-
-    /**
-     * 套餐包含商品
-     */
     @ApiModelProperty("套餐包含商品")
     private String productChildIds;
 
