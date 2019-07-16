dadf5a98dc685a2efad50d0af6386477955801e2
litao <762118752@qq.com>
2019-07-16 16:52:51 +0800

    完善学校后台物理卡号查询

---
+++ bdnav-web/client-web/src/main/java/com/bdxh/client/controller/wallet/PhysicalCardWebController.java
@@ -0,0 +1,98 @@
+package com.bdxh.client.controller.wallet;
+
+import com.bdxh.client.configration.security.utils.SecurityUtils;
+import com.bdxh.common.utils.wrapper.WrapMapper;
+import com.bdxh.school.entity.SchoolUser;
+import com.bdxh.wallet.dto.AddPhysicalCard;
+import com.bdxh.wallet.dto.ModifyPhysicalCard;
+import com.bdxh.wallet.dto.QueryPhysicalCard;
+import com.bdxh.wallet.entity.PhysicalCard;
+import com.bdxh.wallet.feign.PhysicalCardControllerClient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/physicalCardWeb")
+@Slf4j
+@Validated
+@Api(value = "物理卡控制器", tags = "物理卡控制器")
+public class PhysicalCardWebController {
+
+    @Autowired
+    private PhysicalCardControllerClient physicalCardControllerClient;
+
+
+
+    @ApiOperation(value = "删除物理卡号",response = Boolean.class)
+    @RequestMapping(value = "/removePhysicalCard", method = RequestMethod.GET)
+    public Object removePhysicalCard(@RequestParam("schoolCode") String schoolCode, @RequestParam("cardNumber") String cardNumber,@RequestParam("id") Long id) {
+        try {
+            return physicalCardControllerClient.delPhysicalCard(schoolCode,cardNumber,id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+    @ApiOperation(value = "添加物理卡号",response = Boolean.class)
+    @RequestMapping(value = "/addPhysicalCard", method = RequestMethod.POST)
+    public Object addPhysicalCard(@RequestBody AddPhysicalCard addPhysicalCard) {
+        try {
+            SchoolUser user = SecurityUtils.getCurrentUser();
+            addPhysicalCard.setOperator(user.getId());
+            addPhysicalCard.setOperatorName(user.getUserName());
+            return physicalCardControllerClient.addPhysicalCard(addPhysicalCard);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+    @ApiOperation(value="修改物理卡号",response = Boolean.class)
+    @RequestMapping(value = "/modifyPhysicalCard", method = RequestMethod.POST)
+    public Object modifyPhysicalCard(@RequestBody ModifyPhysicalCard modifyPhysicalCard) {
+        try {
+            SchoolUser user = SecurityUtils.getCurrentUser();
+            modifyPhysicalCard.setOperator(user.getId());
+            modifyPhysicalCard.setOperatorName(user.getUserName());
+            return physicalCardControllerClient.modifyPhysicalCard(modifyPhysicalCard);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+    @ApiOperation(value = "分页查询物理卡号",response = PhysicalCard.class)
+    @RequestMapping(value = "/findPhysicalCardInCondition", method = RequestMethod.POST)
+    public Object findPhysicalCardInCondition(@RequestBody QueryPhysicalCard queryPhysicalCard) {
+        try {
+            SchoolUser user = SecurityUtils.getCurrentUser();
+            queryPhysicalCard.setSchoolCode(user.getSchoolCode());
+            return physicalCardControllerClient.findPhysicalCardInCondition(queryPhysicalCard);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "根据id查询物理卡号",response = PhysicalCard.class)
+    @RequestMapping(value = "/findPhysicalCardById", method = RequestMethod.GET)
+    public Object findPhysicalCardById(@RequestParam("schoolCode") String schoolCode, @RequestParam("cardNumber") String cardNumber,@RequestParam("id") Long id) {
+        try {
+            return physicalCardControllerClient.findPhysicalCardById(schoolCode,cardNumber,id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+
+}
---
+++ bdnav-web/client-web/src/main/java/com/bdxh/client/controller/wallet/WalletAccountWebController.java
@@ -0,0 +1,86 @@
+package com.bdxh.client.controller.wallet;
+
+import com.bdxh.client.configration.security.utils.SecurityUtils;
+import com.bdxh.common.utils.wrapper.WrapMapper;
+import com.bdxh.school.entity.SchoolUser;
+import com.bdxh.wallet.dto.AddWalletAccount;
+import com.bdxh.wallet.dto.ModifyWalletAccount;
+import com.bdxh.wallet.dto.QueryWalletAccount;
+import com.bdxh.wallet.entity.WalletAccount;
+import com.bdxh.wallet.feign.WalletAccountControllerClient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/walletAccountWeb")
+@Slf4j
+@Validated
+@Api(value = "钱包账户控制器", tags = "钱包账户控制器")
+public class WalletAccountWebController {
+
+    @Autowired
+    private WalletAccountControllerClient walletAccountControllerClient;
+
+
+
+    @ApiOperation(value = "删除钱包账户",response = Boolean.class)
+    @RequestMapping(value = "/delWalletAccount", method = RequestMethod.GET)
+    public Object delWalletAccount(@RequestParam("schoolCode") String schoolCode, @RequestParam("cardNumber") String cardNumber, @RequestParam("id") Long id) {
+        try {
+            return walletAccountControllerClient.delWalletAccount(schoolCode,cardNumber,id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+    @ApiOperation(value = "添加钱包账号",response = Boolean.class)
+    @RequestMapping(value = "/addWalletAccount", method = RequestMethod.POST)
+    public Object addWalletAccount(@RequestBody AddWalletAccount addWalletAccount) {
+        try {
+            SchoolUser user = SecurityUtils.getCurrentUser();
+            addWalletAccount.setOperator(user.getId());
+            addWalletAccount.setOperatorName(user.getUserName());
+            return walletAccountControllerClient.addWalletAccount(addWalletAccount);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+    @ApiOperation(value="修改钱包账号",response = Boolean.class)
+    @RequestMapping(value = "/modifyPhysicalCard", method = RequestMethod.POST)
+    public Object modifyPhysicalCard(@RequestBody ModifyWalletAccount modifyWalletAccount) {
+        try {
+            SchoolUser user = SecurityUtils.getCurrentUser();
+            modifyWalletAccount.setOperator(user.getId());
+            modifyWalletAccount.setOperatorName(user.getUserName());
+            return walletAccountControllerClient.modifyWalletAccount(modifyWalletAccount);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+    @ApiOperation(value = "分页查询钱包账号",response = WalletAccount.class)
+    @RequestMapping(value = "/findWalletAccountInCondition", method = RequestMethod.POST)
+    public Object findWalletAccountInCondition(@RequestBody QueryWalletAccount queryWalletAccount) {
+        try {
+            SchoolUser user = SecurityUtils.getCurrentUser();
+            queryWalletAccount.setSchoolCode(user.getSchoolCode());
+            return walletAccountControllerClient.findWalletAccountInCondition(queryWalletAccount);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return WrapMapper.error(e.getMessage());
+        }
+    }
+
+
+}
