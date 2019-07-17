aaf9a217d5d49f53303cacbc7fc7eda93059abdb
litao <762118752@qq.com>
2019-07-17 12:24:06 +0800

    完善学校后台物理

--- bdnav-web/client-web/src/main/java/com/bdxh/client/controller/wallet/WalletAccountWebController.java
+++ bdnav-web/client-web/src/main/java/com/bdxh/client/controller/wallet/WalletAccountWebController.java
@@ -73,7 +73,8 @@ public class WalletAccountWebController {
     @RequestMapping(value = "/findWalletAccountInCondition", method = RequestMethod.POST)
     public Object findWalletAccountInCondition(@RequestBody QueryWalletAccount queryWalletAccount) {
         try {
- /*           SchoolUser user = SecurityUtils.getCurrentUser();*/
+            SchoolUser user = SecurityUtils.getCurrentUser();
+            queryWalletAccount.setSchoolCode(user.getSchoolCode());
             return walletAccountControllerClient.findWalletAccountInCondition(queryWalletAccount);
         } catch (Exception e) {
             e.printStackTrace();
